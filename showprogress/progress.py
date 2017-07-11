2fe0d4be009732bfc2dbc227ed4a8e6ac27d9fad
lovit <soy.lovit@gmail.com>
2017-07-11 15:15:28 +0900

    Minor change

--- showprogress/progress.py
+++ showprogress/progress.py
@@ -5,7 +5,7 @@ import time
 import sys
 import datetime
 
-def progress(iterable, head=None, item_length=0, memory_usage=True, memory_available=False, check_points=1):
+def progress(iterable, head=None, item_length=0, memory_usage=True, memory_available=False, check_points=1, bar_character='#'):
     begin_time = time.time()
     n = len(iterable)
     
@@ -20,8 +20,8 @@ def progress(iterable, head=None, item_length=0, memory_usage=True, memory_avail
         process_time = time.time() - begin_time
         remained_time = (n - i) * process_time / (i+1) 
         
-        args = (head+': ' if head else '', '█'*n_block, '-'*n_bar, 100*i/n, '%', datetime.timedelta(seconds=int(process_time)), datetime.timedelta(seconds=int(remained_time)))
-        message = '%s|%s%s| %.3f %s, [%s < %s]  ' % args
+        args = (head+': ' if head else '', bar_character*n_block, '-'*n_bar, 100*i/n, '%', datetime.timedelta(seconds=int(process_time)), datetime.timedelta(seconds=int(remained_time)))
+        message = '%s|%s%s| %.3f %s, [- %s, + %s]  ' % args
         
         if item_length > 0:
             message += (str(item)[:item_length] + '  ')
@@ -31,5 +31,5 @@ def progress(iterable, head=None, item_length=0, memory_usage=True, memory_avail
             message += ('%.3f Gb available  ' % get_available_memory())
         sys.stdout.write('\r%s' % message.strip())
         
-    print('\r%sdone' % (head + ' ' if head else ''))
+    print('\r%sdone. process time: %s' % (head + ' ' if head else '', datetime.timedelta(seconds=int(process_time))))
         
